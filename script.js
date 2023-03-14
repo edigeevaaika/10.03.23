ba5e13daef8ac86960af2a9bde32bb5ef85a960d
Aida <edigeeva.aika@mail.ru>
2023-03-14 13:01:39 +0600

    added functions

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ const addItemBtn = document.querySelector("#add-item");
 function removeItem() {
   this.parentElement.remove();
 }
+function addItem() {
+  // проверить наличие значения input
+  if (newItemInpt.value.trim() !== "") {
+    // добавить li к ul
+    const itemLi = document.createElement("li");
+    itemsList.append(itemLi);
+  
+    // задать содержимое li исходя из значения input
+    itemLi.textContent = newItemInpt.value;
+  
+    // сбросить значение input
+    newItemInpt.value = "";
+    // добавить button к li
+    const removeBtn = document.createElement("button");
+    itemLi.append(removeBtn);
+    // задать текст и слушатель событий для button
+    removeBtn.textContent = "Remove";
+    removeBtn.addEventListener("click", removeItem);
+  }
+}
